848264b30014378186aedab4d7fd74f3871361ff
Shelwin <shelwinsunga@gmail.com>
2024-11-10 12:03:01 -0800

    update nix version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "code editor";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
     flake-utils.url = "github:numtide/flake-utils";
     zig = {
       url = "github:mitchellh/zig-overlay";
